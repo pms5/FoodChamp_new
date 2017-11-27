d3be59f374bbc157654abf0e10436a3bb1039193
Bryan Schoot <Bryan Schoot>
2017-11-27 15:27:50 +0100

    Commit layout

--- resources/views/home/index.php
+++ resources/views/home/index.php
@@ -3,6 +3,40 @@
 </head>
 <body>
 <!-- Image and text -->
-<nav>
-
+<nav class="navbar sticky-top navbar-light bg-palletlightGrey">
+    <a class="navbar-brand h-nav" href="#">
+        <div class="position-absolute">
+            <img src="img/logo/2.png" width="auto" height="100" class="d-inline-block align-top" alt="">
+        </div>
+    </a>
 </nav>
+
+<img src="img/home/1.png" class="img-fluid" alt="Responsive image">
+
+<div class="container py-5">
+    <div class="card-deck">
+        <div class="card">
+            <div class="text-center">
+                <i class="fa fa-paper-plane fa-5x py-2" aria-hidden="true"></i>
+                <div class="card-body">
+                    <h4 class="card-title">Fast delivery</h4>
+                    <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                </div>
+            </div>
+        </div>
+        <div class="card">
+            <div class="card-body">
+                <h4 class="card-title">Card title</h4>
+                <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+                <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+            </div>
+        </div>
+        <div class="card">
+            <div class="card-body">
+                <h4 class="card-title">Card title</h4>
+                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+                <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+            </div>
+        </div>
+    </div>
+</div>
